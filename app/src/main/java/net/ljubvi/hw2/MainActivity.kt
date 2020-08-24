6d84976f3f1a1bdee90ab57bcc3101ce85f4f8fb
Andis Jurjans <a@ljubvi.net>
2020-08-24 19:10:50 +0300

    interaktīva poga ar kaunteri

--- app/src/main/java/net/ljubvi/hw2/MainActivity.kt
+++ app/src/main/java/net/ljubvi/hw2/MainActivity.kt
@@ -2,13 +2,32 @@ package net.ljubvi.hw2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this,"Welcome, dear clicker!",Toast.LENGTH_LONG).show()
+        val poga: Button = findViewById(R.id.Poga)
+        val teksts: TextView = findViewById(R.id.textView1)
+        var clicks = 0
+        poga.text = "Spaidi mani!"
+        teksts.text = "Vēl nau spaidīc!"
+        poga.setOnClickListener{clicks++; teksts.text = "$clicks"; Toast.makeText(this,"$clicks",Toast.LENGTH_SHORT).show() }
+
+
+
+
+
 
     }
+
+
 }
+
+
 
