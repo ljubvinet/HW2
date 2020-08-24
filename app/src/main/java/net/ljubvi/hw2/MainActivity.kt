e27806295080f910b5308d4b95ac31fad3967486
Andis Jurjans <a@ljubvi.net>
2020-08-24 19:21:43 +0300

    Logošana

--- app/src/main/java/net/ljubvi/hw2/MainActivity.kt
+++ app/src/main/java/net/ljubvi/hw2/MainActivity.kt
@@ -2,14 +2,16 @@ package net.ljubvi.hw2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
         Toast.makeText(this,"Welcome, dear clicker!",Toast.LENGTH_LONG).show()
         val poga: Button = findViewById(R.id.Poga)
@@ -17,7 +19,14 @@ class MainActivity : AppCompatActivity() {
         var clicks = 0
         poga.text = "Spaidi mani!"
         teksts.text = "Vēl nau spaidīc!"
-        poga.setOnClickListener{clicks++; teksts.text = "$clicks"; Toast.makeText(this,"$clicks",Toast.LENGTH_SHORT).show() }
+
+        poga.setOnClickListener{
+            clicks++;
+            teksts.text = "$clicks"
+            Toast.makeText(this,"$clicks",Toast.LENGTH_SHORT).show()
+            Log.i(TAG,"klikšķis ir")
+            Log.i(TAG,"$clicks")
+        }
 
 
 
